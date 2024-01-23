5c997d4cf61e5b2c44ad148eb60f69571e369885
Luigi-Faldetta <luigifaldetta@gmail.com>
2024-01-23 17:58:11 +0100

    feat: removed facebook provider

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -5,7 +5,6 @@ import {
   type NextAuthOptions,
 } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import FacebookProvider from "next-auth/providers/facebook";
 
 import { env } from "~/env";
 import { db } from "~/server/db";
@@ -51,10 +50,6 @@ export const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    }),
-    FacebookProvider({
-      clientId: process.env.FACEBOOK_CLIENT_ID!,
-      clientSecret: process.env.FACEBOOK_CLIENT_SECRET!,
     }),
     /**
      * ...add more providers here.
