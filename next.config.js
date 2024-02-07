4e4d151f23b06492d937fafc03525b7c9305a13d
Luigi-Faldetta <luigifaldetta@gmail.com>
2024-02-07 19:29:53 +0100

    feat: updated html and background

--- next.config.js
+++ next.config.js
@@ -5,6 +5,26 @@
 await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
-const config = {};
+
+const config = {
+  webpack(config, { isServer }) {
+    const prefix = config.assetPrefix ?? config.basePath ?? "";
+    config.module.rules.push({
+      test: /\.mp4$/,
+      use: [
+        {
+          loader: "file-loader",
+          options: {
+            publicPath: `${prefix}/_next/static/media/`,
+            outputPath: `${isServer ? "../" : ""}static/media/`,
+            name: "[name].[hash].[ext]",
+          },
+        },
+      ],
+    });
+
+    return config;
+  },
+};
 
 export default config;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,82 +7,24 @@ import { api } from "~/trpc/server";
 
 export default async function Home() {
   noStore();
-  const hello = await api.post.hello.query({ text: "from tRPC" });
+  const hello = await api.post.hello.query({ text: "from BuddyApp" });
   const session = await getServerAuthSession();
   console.log("session:", session?.user);
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+    <main className="flex min-h-screen flex-col items-center justify-center">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-        <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-          Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
-        </h1>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-            href="https://create.t3.gg/en/usage/first-steps"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">First Steps →</h3>
-            <div className="text-lg">
-              Just the basics - Everything you need to know to set up your
-              database and authentication.
-            </div>
-          </Link>
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-            href="https://create.t3.gg/en/introduction"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">Documentation →</h3>
-            <div className="text-lg">
-              Learn more about Create T3 App, the libraries it uses, and how to
-              deploy it.
-            </div>
-          </Link>
-        </div>
         <div className="flex flex-col items-center gap-2">
+          <video
+            autoPlay
+            loop
+            muted
+            src="/_import_8614f0198d20807.91890252.mp4"
+            className="absolute left-0 top-0 -z-10 h-screen w-screen object-cover"
+          ></video>
           <p className="text-2xl text-white">
             {hello ? hello.greeting : "Loading tRPC query..."}
           </p>
-
-          <form className="mt-6">
-            <div className="mb-4">
-              <label
-                htmlFor="email"
-                className="block text-sm font-semibold text-white"
-              >
-                Email
-              </label>
-              <input
-                type="email"
-                className="mt-2 block w-full rounded-md border bg-white px-4 py-2 text-gray-700 focus:border-gray-400 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-40"
-              />
-            </div>
-            <div className="mb-2">
-              <label
-                htmlFor="password"
-                className="block text-sm font-semibold text-white"
-              >
-                Password
-              </label>
-              <input
-                type="password"
-                className="mt-2 block w-full rounded-md border bg-white px-4 py-2 text-gray-700 focus:border-gray-400 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-40"
-              />
-            </div>
-            <Link
-              href="/signup"
-              className="text-xs text-blue-600 hover:underline"
-            >
-              Forgot your Password?
-            </Link>
-            <div className="mt-2">
-              <button className="w-full transform rounded-md bg-red-500 px-4 py-2 font-semibold tracking-wide text-white transition-colors duration-200 hover:bg-red-400 hover:text-slate-800 focus:bg-red-500 focus:outline-none">
-                Login
-              </button>
-            </div>
-          </form>
 
           <div className="flex flex-col items-center justify-center gap-4">
             <p className="text-center text-2xl text-white">
